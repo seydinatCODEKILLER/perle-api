89681e052d5bcddb68d2d730bb35514fa306ffcc
Seydina Thiam <seydinat235@gmail.com>
2026-02-24 14:32:39 +0000

    mise en place de origin

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());  
 app.use(cors({
-  origin: true,
+  origin: "https://perle-front.vercel.app/",
   credentials: true,
 }));
 app.use(httpLogger);
